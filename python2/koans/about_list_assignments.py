0ef010c35a878ec336198fd4432058944d0b2aca
Yashwanth <yashwanthbabu.gujarathi@gmail.com>
2014-07-17 19:18:23 +0530

    Update about_list_assignments.py

--- python2/koans/about_list_assignments.py
+++ python2/koans/about_list_assignments.py
@@ -11,21 +11,21 @@ from runner.koan import *
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
         names = ["John", "Smith"]
-        self.assertEqual(__, names)
+        self.assertEqual(["John", "Smith"], names)
 
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(first_name[:4], first_name)
+        self.assertEqual(last_name[0:], last_name)
 
     def test_parallel_assignments_with_sublists(self):
         first_name, last_name = [["Willie", "Rae"], "Johnson"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(first_name[0:2], first_name)
+        self.assertEqual(last_name[0:], last_name)
 
     def test_swapping_with_parallel_assignment(self):
         first_name = "Roy"
         last_name = "Rob"
         first_name, last_name = last_name, first_name
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(first_name[:3], first_name)
+        self.assertEqual(last_name[:3], last_name)
