0055e044a504e37fa96359e1bdb161fbad1acf78
Yashwanth <yashwanthbabu.gujarathi@gmail.com>
2014-07-18 10:07:18 +0530

    Update about_new_style_classes.py

--- python2/koans/about_new_style_classes.py
+++ python2/koans/about_new_style_classes.py
@@ -34,7 +34,7 @@ class AboutNewStyleClasses(Koan):
     # ------------------------------------------------------------------
 
     def test_old_style_classes_have_type_but_no_class_attribute(self):
-        self.assertEqual(__, type(self.OldStyleClass).__name__)
+        self.assertEqual('classobj', type(self.OldStyleClass).__name__)
 
         try:
             cls = self.OldStyleClass.__class__.__name__
